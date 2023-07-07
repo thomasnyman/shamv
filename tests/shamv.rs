07e411ef52f207b0be6ad81a0f643cd171323852
Thomas Nyman <thomas.nyman@iki.fi>
2023-07-07 16:16:09 +0300

    Add SHA-224 algorithm and test vectors

--- tests/shamv.rs
+++ tests/shamv.rs
@@ -99,6 +99,140 @@ fn file_without_extension() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+fn sha224_of_empty_file() -> Result<(), Box<dyn std::error::Error>> {
+    let old_file_name = "empty.txt";
+    let new_file_name = "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f.txt";
+
+    let tmp_dir = assert_fs::TempDir::new()?;
+    let file = tmp_dir.child(old_file_name);
+    file.write_str("")?;
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_CRATE_NAME"))?;
+    cmd.arg("--dry-run").arg("--algorithm").arg("sha224").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(new_file_name));
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_CRATE_NAME"))?;
+    cmd.arg("--algorithm").arg("sha224").arg(file.path());
+    cmd.assert()
+        .success();
+
+    tmp_dir
+        .child(new_file_name)
+        .assert(predicate::path::exists());
+
+    Ok(())
+}
+
+#[test]
+fn sha224_of_nist_1_test_vector() -> Result<(), Box<dyn std::error::Error>> {
+    let old_file_name = "NIST.1.txt";
+    let new_file_name = "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7.txt";
+
+    let tmp_dir = assert_fs::TempDir::new()?;
+    let file = tmp_dir.child(old_file_name);
+    file.write_str("abc")?;
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_CRATE_NAME"))?;
+    cmd.arg("--dry-run").arg("--algorithm").arg("sha224").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(new_file_name));
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_CRATE_NAME"))?;
+    cmd.arg("--algorithm").arg("sha224").arg(file.path());
+    cmd.assert()
+        .success();
+
+    tmp_dir
+        .child(new_file_name)
+        .assert(predicate::path::exists());
+
+    Ok(())
+}
+
+#[test]
+fn sha224_of_nist_3_test_vector() -> Result<(), Box<dyn std::error::Error>> {
+    let old_file_name = "NIST.3.txt";
+    let new_file_name = "20794655980c91d8bbb4c1ea97618a4bf03f42581948b2ee4ee7ad67.txt";
+
+    let tmp_dir = assert_fs::TempDir::new()?;
+    let file = tmp_dir.child(old_file_name);
+    file.write_str(&"a".repeat(1000000))?;
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_CRATE_NAME"))?;
+    cmd.arg("--dry-run").arg("--algorithm").arg("sha224").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(new_file_name));
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_CRATE_NAME"))?;
+    cmd.arg("--algorithm").arg("sha224").arg(file.path());
+    cmd.assert()
+        .success();
+
+    tmp_dir
+        .child(new_file_name)
+        .assert(predicate::path::exists());
+
+    Ok(())
+}
+
+#[test]
+fn sha224_of_multiple_input_files() -> Result<(), Box<dyn std::error::Error>> {
+    let old_empty_file_name = "empty.txt";
+    let new_empty_file_name = "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f.txt";
+    let old_nist_1_file_name = "NIST.1.txt";
+    let new_nist_1_file_name = "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7.txt";
+    let old_nist_3_file_name = "NIST.3.txt";
+    let new_nist_3_file_name = "20794655980c91d8bbb4c1ea97618a4bf03f42581948b2ee4ee7ad67.txt";
+
+    let tmp_dir = assert_fs::TempDir::new()?;
+    let empty_file = tmp_dir.child(old_empty_file_name);
+    empty_file.write_str("")?;
+    let nist_1_file = tmp_dir.child(old_nist_1_file_name);
+    nist_1_file.write_str("abc")?;
+    let nist_3_file = tmp_dir.child(old_nist_3_file_name);
+    nist_3_file.write_str(&"a".repeat(1000000))?;
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_CRATE_NAME"))?;
+    cmd.arg("--dry-run").arg("--algorithm").arg("sha224")
+        .arg(empty_file.path())
+        .arg(nist_1_file.path())
+        .arg(nist_3_file.path());
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(new_empty_file_name))
+        .stdout(predicate::str::contains(new_nist_1_file_name))
+        .stdout(predicate::str::contains(new_nist_3_file_name));
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_CRATE_NAME"))?;
+    cmd.arg("--algorithm").arg("sha224")
+        .arg(empty_file.path())
+        .arg(nist_1_file.path())
+        .arg(nist_3_file.path());
+
+    cmd.assert()
+        .success();
+
+    tmp_dir
+        .child(new_empty_file_name)
+        .assert(predicate::path::exists());
+
+    tmp_dir
+        .child(new_nist_1_file_name)
+        .assert(predicate::path::exists());
+
+    tmp_dir
+        .child(new_nist_3_file_name)
+        .assert(predicate::path::exists());
+
+    Ok(())
+}
+
+#[test]
 fn sha256_of_empty_file() -> Result<(), Box<dyn std::error::Error>> {
     let old_file_name = "empty.txt";
     let new_file_name = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.txt";
